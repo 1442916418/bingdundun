4b1ec48e8d08bbfbb5fd2f22fa191dbb0446294a
yushuisheng <1442916418@qq.com>
2022-02-09 23:21:25 +0800

    增加右手手

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const m = 2
 const w = 550 * m
 const h = 660 * m
+const c0 = '#D3D3D3'
 const c1 = '#2D2D2D'
+const c2 = 'transparent'
+const colorList = ['#ADFF2F', '#FFD700', '#FF4500', '#6A5ACD', '#00FFFF']
+
 const canvas = document.querySelector('canvas')
 
 canvas.width = w
@@ -15,57 +19,147 @@ c.fillRect(0, 0, w, h)
 // 身体
 drarRroundRect({
   c,
-  x: getValue(150),
-  y: getValue(120),
-  w: getValue(280),
-  h: getValue(300),
-  r: getValue(130),
+  x: gv(150),
+  y: gv(120),
+  w: gv(280),
+  h: gv(300),
+  r: gv(130),
 })
 
 // 左侧耳朵
 drawSemicircle({
   c,
-  x: getValue(100),
-  y: getValue(184),
-  w: getValue(85),
-  r: getValue(40),
+  x: gv(70),
+  y: gv(195),
+  w: gv(85),
+  r: gv(40),
   fillStyle: '#FFFFFF',
-  angle: -25.5 * Math.PI / 180
+  angle: -30.5 * Math.PI / 180
 })
 
 // 内左侧耳朵
 drawSemicircle({
   c,
-  x: getValue(112),
-  y: getValue(197),
-  w: getValue(50),
-  r: getValue(25),
+  x: gv(85),
+  y: gv(210),
+  w: gv(50),
+  r: gv(25),
   strokeStyle: 'transparent',
   fillStyle: c1,
   angle: -1.5 * Math.PI / 180
 })
 
-resetRotate(c, 27 * Math.PI / 180)
+resetRotate(c, 32 * Math.PI / 180)
 
-// 右侧内耳朵
+// 右侧耳朵
 drawSemicircle({
   c,
-  x: getValue(350),
-  y: getValue(-164),
-  w: getValue(85),
-  r: getValue(40),
+  x: gv(345),
+  y: gv(-144),
+  w: gv(85),
+  r: gv(40),
   fillStyle: '#FFFFFF',
-  angle: 40.1 * Math.PI / 180
+  angle: 37 * Math.PI / 180
 })
 
 // 右侧内耳朵
 drawSemicircle({
   c,
-  x: getValue(362),
-  y: getValue(-155),
-  w: getValue(50),
-  r: getValue(25),
+  x: gv(360),
+  y: gv(-140),
+  w: gv(50),
+  r: gv(25),
   strokeStyle: 'transparent',
   fillStyle: c1,
   angle: 1 * Math.PI / 180
+})
+
+resetRotate(c, -38 * Math.PI / 180)
+
+// 右手
+drawSemicircle({
+  c,
+  x: gv(430),
+  y: gv(210),
+  w: gv(55),
+  r: gv(25),
+})
+
+// 右胳膊
+drawArcTo({
+  c,
+  x: gv(485.5),
+  y: gv(247),
+  x0: gv(485.5),
+  y0: gv(297),
+  x1: gv(448.5),
+  y1: gv(312),
+  r: 185,
+})
+
+// 右手（内）
+drawSemicircle({
+  c,
+  x: gv(430),
+  y: gv(225),
+  w: gv(45),
+  r: gv(20),
+  strokeStyle: c2,
+  fillStyle: c1
+})
+
+// 右胳膊(内)
+c.beginPath()
+c.moveTo(gv(430), gv(240))
+c.lineTo(gv(476), gv(240))
+c.lineTo(gv(431), gv(293.5))
+c.fillStyle = c1
+c.fill()
+c.closePath()
+
+drawArcTo({
+  c,
+  x: gv(475),
+  y: gv(240),
+  x0: gv(475),
+  y0: gv(280),
+  x1: gv(410),
+  y1: gv(300),
+  r: 125,
+  strokeStyle: c2,
+  fillStyle: c1
+})
+
+// 右胳膊(内) 爱心
+c.beginPath()
+c.font = '50px 微软雅黑'
+c.fillStyle = '#FF4500'
+c.fillText('❤', gv(437), gv(260))
+c.closePath()
+
+// TODO: 先画中间部分
+
+colorList.forEach((color, i) => {
+  // let cw = gv(i ? 80 + (i * 4) : 80)
+
+  // drawCircular({
+  //   c,
+  //   x: gv(290),
+  //   y: gv(250),
+  //   w: cw,
+  //   h: gv(i ? 50 + (i * 4) : 50),
+  //   strokeStyle: color
+  // })
+
+  let cw = gv(i ? 175 + (i * 4) : 175)
+  drarRroundRect({
+    c,
+    x: gv(200),
+    y: gv(160),
+    w: cw,
+    h: gv(i ? 140 + (i * 4) : 140),
+    r: gv(78),
+    strokeStyle: color,
+    lineWidth: 3
+  })
 })
--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
-const getValue = (v, m = 2) => v * m
+/**
+ * @name: 获取倍数值
+ * @param {Number} v - 原值
+ * @param {Number} m - 放大倍数
+ */
+const gv = (v, m = 2) => v * m
 
 /**
  * @name: 重置旋转角度
@@ -15,6 +20,7 @@ const resetRotate = (c, angle) => c.rotate(angle)
  * @param {Number} w - 矩形的宽度
  * @param {Number} h - 矩形的高度
  * @param {Number} r - 矩形的圆角半径
+ * @param {lineWidth} r - 矩形的边框宽度
  * @param {String} [strokeStyle = '#D3D3D3'] - 矩形的边框色
  * @param {String} [fillStyle = 'transparent']   - 矩形的填充色
  */
@@ -26,12 +32,13 @@ const drarRroundRect = ({
   h,
   r,
   strokeStyle = '#D3D3D3',
-  fillStyle = "transparent"
+  fillStyle = "transparent",
+  lineWidth = 2
 } = {}) => {
   c.beginPath()
   c.strokeStyle = strokeStyle
   c.fillStyle = fillStyle
-  c.lineWidth = getValue(2)
+  c.lineWidth = gv(lineWidth)
 
   // 左上半圆
   c.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
@@ -81,13 +88,109 @@ const drawSemicircle = ({
   c.beginPath()
   c.strokeStyle = strokeStyle
   c.fillStyle = fillStyle
-  c.lineWidth = getValue(2)
+  c.lineWidth = gv(2)
 
   // 左上半圆
   c.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
 
   // 右上半圆
   c.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2)
+
+  c.fill()
+  c.stroke()
+  c.closePath()
+}
+
+/**
+ * 绘制弧线
+ * @param {Object} c - canvas 组件的绘图上下文
+ * @param {Number} x - 横坐标
+ * @param {Number} y - 纵坐标
+ * @param {Number} x0 - 两切线交点的横坐标
+ * @param {Number} y0 - 两切线交点的纵坐标
+ * @param {Number} x1 - 第二条切线上一点的横坐标
+ * @param {Number} y1 - 第二条切线上一点的纵坐标
+ * @param {Number} r - 半径
+ * @param {String} [strokeStyle = '#D3D3D3'] - 矩形的边框色
+ * @param {String} [fillStyle = '#FFFFFF']   - 矩形的填充色
+ */
+const drawArcTo = ({
+  c,
+  x,
+  y,
+  x0,
+  y0,
+  x1,
+  y1,
+  r,
+  strokeStyle = '#D3D3D3',
+  fillStyle = "#FFFFFF",
+} = {}) => {
+  c.beginPath();
+  c.strokeStyle = strokeStyle
+  c.fillStyle = fillStyle
+  c.lineWidth = gv(2)
+
+  c.moveTo(x, y);
+  c.arcTo(x0, y0, x1, y1, r);
+
+  c.fill()
+  c.stroke()
+  c.closePath()
+}
+
+/**
+ * 绘制矩形
+ * @param {Object} c - canvas 组件的绘图上下文
+ * @param {Number} x - 矩形的 x 坐标
+ * @param {Number} y - 矩形的 y 坐标
+ * @param {Number} w - 矩形的宽度
+ * @param {Number} h - 矩形的高度
+ * @param {String} [fillStyle = 'transparent']   - 矩形的填充色
+ */
+const drawRect = ({
+  c,
+  x,
+  y,
+  w,
+  h,
+  fillStyle = "#FFFFFF"
+} = {}) => {
+  c.beginPath()
+  c.fillStyle = fillStyle
+
+  c.fillRect(x, y, w, h)
+
+  c.fill()
+  c.closePath()
+}
+
+
+/**
+ * 绘制圆形
+ * @param {Object} c - canvas 组件的绘图上下文
+ * @param {Number} x - 圆形的 x 坐标
+ * @param {Number} y - 圆形的 y 坐标
+ * @param {Number} w - 圆形的宽度
+ * @param {Number} h - 圆形的高度
+ * @param {String} [fillStyle = 'transparent'] - 圆形的填充色
+ * @param {String} [strokeStyle = '#D3D3D3'] - 矩形的边框色
+ */
+const drawCircular = ({
+  c,
+  x,
+  y,
+  w,
+  h,
+  fillStyle = "transparent",
+  strokeStyle = '#D3D3D3'
+} = {}) => {
+  c.beginPath()
+  c.fillStyle = fillStyle
+  c.strokeStyle = strokeStyle
+  c.lineWidth = gv(3)
+
+  c.arc(x, y, w, h, 0, 2 * Math.PI)
 
   c.fill()
   c.stroke()
